42b2ea06624520d2a48cc711013f09fb97bbe56b
Amaury Rodriguez <iamauryrov93@gmail.com>
2018-08-14 12:22:16 -0500

    Logs added in CourseController and CourseControllerService

--- src/main/java/com/udemy/backendninja/controller/CourseController.java
+++ src/main/java/com/udemy/backendninja/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.udemy.backendninja.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -17,24 +19,29 @@ import com.udemy.backendninja.service.CourseService;
 public class CourseController {
 
 	private static final String COURSES_VIEW = "courses";
-	
+	private static final Log LOG = LogFactory.getLog(CourseController.class);
+
 	@Autowired
 	@Qualifier("CourseServiceImpl")
 	private CourseService courseService;
-	
-	@GetMapping(value= "/listCourses")
-	public ModelAndView listCourses(){
+
+	@GetMapping(value = "/listCourses")
+	public ModelAndView listCourses() {
+		LOG.info("Call: listAllCourses()");
+
 		ModelAndView model = new ModelAndView(COURSES_VIEW);
 		model.addObject("courses", courseService.listAllCourses());
 		return model;
 	}
-	
+
 	@PostMapping(value = "/addCourse")
 	public String postMethodName(@ModelAttribute("course") Course course) {
 		courseService.addCourse(course);
-		
-		//Finally we will show the new course added in the listCourses view.
-		return "redirect:/listCourses";
+		LOG.info("Call: addCourse()");
+		LOG.info("Course added. New course: " + course.toString());
+
+		// Finally we will show the new course added in the listCourses view.
+		return "redirect:/course/listCourses";
 	}
 
 }
